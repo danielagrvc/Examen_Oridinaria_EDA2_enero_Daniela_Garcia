d9abe6d828089a7f994d02e4fa773dd634bcb043
danielagrvc <danielagrvc@gmail.com>
2023-01-09 16:47:44 +0100

    Ej4.py

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,1 +1,19 @@
-Class artefactorvaliosos
+#Primer_apartado
+Class ArtefactorValiosos:
+    def __init__ (self, peso, nombre, precio, fecha_caducidad)
+        yomismo.peso = peso
+        yomismo.nombre = nombre
+        yomismo.precio = precio
+        yomismo.fecha_caducidad = fecha_caducidad
+        print("La conserva se ha creado con exito")
+
+def __str__(self):
+    cadena= "PESO" + str(self.peso) + "NOMBRE" + str(self.nombre) + "PRECIO" + str(self.precio) + "FECHA_CADUCIDAD" + str(fechaCcaducidad)
+    return cadena
+
+#Segundo_apartado
+
+artefacto1= ArtefactosValiosos(7, "Mesa",3,"03/11/2022")
+artefacto1= ArtefactosValiosos(7, "Silla",3,"14/04/2022")
+artefacto1= ArtefactosValiosos(7, "Tenedor",3,"22/01/2022")
+artefacto1= ArtefactosValiosos(7, "Televisión",3,"23/12/2022")
